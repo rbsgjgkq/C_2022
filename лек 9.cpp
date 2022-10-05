fb7956a848a6fbf784f07db85d55fd0d62adce88
rbsgjgkq <113747232+rbsgjgkq@users.noreply.github.com>
2022-10-05 18:37:27 +0800

    Update лек 9.cpp

--- лек 9.cpp
+++ лек 9.cpp
@@ -33,3 +33,29 @@ return 0;
 }=================================================================
 2) Перегрузите бинарный оператор + для класса CreditCard так, чтобы операция С1 + С2 переносила весь баланс с С2 на С1 (добавляла все сумму с С2 к С1 и обнуляла баланс С2)
 ====================================
+ #include <iostream>
+using namespace std;
+class Creditcard 
+{
+public:
+    double Number;
+    double Balance;
+    Creditcard (int a, float b) {Number = a, Balance = b;}
+    
+};    
+    Creditcard operator + (Creditcard C1, Creditcard &C2)
+{
+    
+    C1.Balance=C1.Balance+C2.Balance;
+    C2.Balance = 0;
+    return C1;     
+}
+
+int main()
+    {
+    Creditcard C1(1345, 100);
+    Creditcard C2(1232, 50);
+    C1 = C1 + C2; 
+    cout << C1.Balance << " , " << C2.Balance << endl;
+    return 0;
+}
